a77d5bdc9b0532fe5855515ad3933fa09c20c98c
Luiz Guilherme <luizguilherme011@gmail.com>
2025-12-01 11:55:54 -0300

    Fix Express 5 OPTIONS route crash using RegExp

--- server/index.ts
+++ server/index.ts
@@ -40,7 +40,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
