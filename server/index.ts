7e412526ebe677f893e440d352358f9a3ddc428b
Luiz Guilherme <luizguilherme011@gmail.com>
2025-12-01 11:22:29 -0300

    Fix CORS preflight configuration

--- server/index.ts
+++ server/index.ts
@@ -27,6 +27,7 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+app.options('*', cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
